e15646d0603be59c211d82605da9449be2b201fa
PsammystPrime <samuelprime384@gmail.com>
2024-02-10 23:45:03 +0300

    Add previous button functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const imageArray = ['one', 'two','three' , 'four'];
 
+//Initialize your images
 const one = document.createElement('img')
 one.src = './Gamer_girl.png'
 const two = document.createElement('img')
@@ -8,20 +8,39 @@ const three = document.createElement('img')
 three.src = './exotic.png'
 const four = document.createElement('img')
 four.src = 'Beautiful.jpg'
+const five = document.createElement('img')
+five.src = 'bridge.png'
+//Push them to an array
+const imageArray = [one, two, three, four, five];
 
-const imageArray = [one, two, three, four];
-
-index = 0
-const next = document.querySelector('.next')
-const display = document.querySelector('.display')
-next.addEventListener('click', nextImg)
+const display = document.querySelector('.display');
+const next = document.querySelector('.next');
+next.addEventListener('click', nextImg);
+const prev = document.querySelector('.prev');
+prev.addEventListener('click', prevImg);
+index = 0;
 
 function nextImg(){
-    if (index===3) {
+    if (index === 4) {
         index = 0
     } else {
        index++  
     }
+    display.innerHTML = ''
+    display.appendChild(imageArray[index]) 
+    console.log(index)
 
-    display.appendChild(imageArray[index]) /*imageArray[index]*/
+}
+function prevImg() {
+    if (index === 0) {
+        index = imageArray.length - 1
+    console.log(imageArray.length)
+
+    }
+    else{
+        index --
+    }
+    display.innerHTML = ''
+    display.appendChild(imageArray[index])
+    console.log(index)
 }
