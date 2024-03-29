cec486b682a451b1e759d17deaede0cd8954cfd0
PsammystPrime <samuelprime384@gmail.com>
2024-03-29 14:59:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-//Initialize your images
-const one = document.createElement('img')
-one.src = './Gamer_girl.png'
-const two = document.createElement('img')
-two.src = './alien.jpg'
-const three = document.createElement('img')
-three.src = './exotic.png'
-const four = document.createElement('img')
-four.src = 'Beautiful.jpg'
-const five = document.createElement('img')
-five.src = 'bridge.png'
-const six = document.querySelector('.homeImg')
-//Push them to an array
-const imageArray = [one, two, three, four, five, six];
+function carouselPreview() {
+  const display = document.querySelector(".display");
+  const next = document.querySelector(".next");
+  next.addEventListener("click", nextImg);
+  const prev = document.querySelector(".prev");
+  prev.addEventListener("click", prevImg);
 
-const display = document.querySelector('.display');
-const next = document.querySelector('.next');
-next.addEventListener('click', nextImg);
-const prev = document.querySelector('.prev');
-prev.addEventListener('click', prevImg);
-index = 0;
+  //Initialize your images
+  const one = document.createElement("img");
+  one.src = "./Gamer_girl.png";
+  const two = document.createElement("img");
+  two.src = "./alien.jpg";
+  const three = document.createElement("img");
+  three.src = "./exotic.png";
+  const four = document.createElement("img");
+  four.src = "Beautiful.jpg";
+  const five = document.createElement("img");
+  five.src = "bridge.png";
+  const six = document.querySelector(".homeImg");
+  //Push them to an array
+  const imageArray = [one, two, three, four, five, six];
 
-function nextImg(){
+  index = 0;
+
+  function nextImg() {
     if (index === 5) {
-        index = 0
+      index = 0;
     } else {
-       index++  
+      index++;
     }
-    display.innerHTML = ''
-    display.appendChild(imageArray[index]) 
-}
-function prevImg() {
+    display.innerHTML = "";
+    display.appendChild(imageArray[index]);
+  }
+  function prevImg() {
     if (index === 0) {
-        index = imageArray.length - 1
+      index = imageArray.length - 1;
+    } else {
+      index--;
     }
-    else{
-        index --
-    }
-    display.innerHTML = ''
-    display.appendChild(imageArray[index])
+    display.innerHTML = "";
+    display.appendChild(imageArray[index]);
+  }
+  setInterval(() => {
+    nextImg();
+  }, 3000);
 }
+carouselPreview();
